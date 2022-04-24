1beef1734f2d9c9d7649dbe0ba05f754409c3500
Sami <samovitch360@gmail.com>
2022-04-24 16:35:13 +0100

    pre tailwind set up

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <div class="logo"></div>
+    <ul class="navItems">
+      <!-- <li v-for="item in NavBarItems" :key="NavBarItems.indexOf(item)">1</li> -->
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+      <li>item 4</li>
+      <li>item 5</li>
+    </ul>
+    <div class="search"></div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: {
+    NavBarItems: ["test1", "test2", "test3", "test4", "test5"],
+  },
+};
+</script>
+
+<style>
+nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100px;
+  background-color: orange;
+}
+.logo {
+}
+</style>
